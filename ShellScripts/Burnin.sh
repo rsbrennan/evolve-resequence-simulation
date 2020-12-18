1111a2a5c44fa8fe6a028cfcdca51d9ab552bd7e
Reid Brennan <reid.brennan@gmail.com>
2020-12-18 17:26:44 -0500

    modifying for single gen, multiple reps from same parents

--- ShellScripts/Burnin.sh
+++ ShellScripts/Burnin.sh
@@ -1,40 +1,47 @@
 ## This script is used to generate neutral populations used for the selection experiment.
 ## IMPORTANT: Change the output directory in the first step and the variables in the second step before running.
 ## Run the script on server using: nohup bash Burnin.sh > Burnin.nohup &
-## Note: the number of generations to be included in the burnin process depends on the population size (typically ten times the population size) and needs to be changed in the Burnin.slim file. Please refer to the notes in the .slim file. 
+## Note: the number of generations to be included in the burnin process depends on the population size (typically ten times the population size) and needs to be changed in the Burnin.slim file. Please refer to the notes in the .slim file.
 
 ## Step 1: Ceate directories to store the outputs. Make sure to change the directory name in the first line.
 
-cd /fs/cbsubscb10/storage/rl683/TemporalScan/Simulations # Change this to a directory where you want to store all you simulation outputs.
+cd /users/r/b/rbrennan/evolve-resequence-simulation/simulations  # Change this to a directory where you want to store all you simulation outputs.
 mkdir Burnin
 cd Burnin
 for k in {1..100} # Number of simulation replicates that you want to create.
-do 
+do
     mkdir 'SimRep'$k
 done
 cd ..
 
-## Step 2: Run burnin using SLiM 2. Variables inside the loop are all customizable and can be changed as desired. 
+## Step 2: Run burnin using SLiM 2. Variables inside the loop are all customizable and can be changed as desired.
 
 for k in {1..100} # Set the number of simulation replicates that you want to create.
 do
-	# Set the path to the SLiM program in the next line 
-    ~/Program/SLiM/bin/slim \
+	SECONDS=0
+    # Set the path to the SLiM program in the next line
+    ~/bin/SLiM/slim \
     -d SimRepID=$k  \
     -d Mu=2e-8 \
     -d RecRate=1e-8 \
-    -d LCh=30000000 \
-    -d BurninSize=1000 \
-    -d "BurninPath='/fs/cbsubscb10/storage/rl683/TemporalScan/Simulations/Burnin/'" \
+    -d LCh=20000000 \
+    -d BurninSize=10000 \
+    -d "BurninPath='/users/r/b/rbrennan/evolve-resequence-simulation/simulations/Burnin/'" \
     -d "BurninFilename='Burnin.txt'" \
-    /fs/cbsubscb10/storage/rl683/TemporalScan/SlimScripts/Burnin.slim # Directory to the Burnin.slim file included in the simulation tool.
+    /users/r/b/rbrennan/evolve-resequence-simulation/SlimScripts/Burnin.slim # Directory to the Burnin.slim file included in the simulation tool.
+
+    duration=$SECONDS
+    echo "done with replicat ${k}"
+    echo "Time to run replicate ${k}: $(($duration / 60)) minutes and $(($duration % 60)) seconds"
+    echo " "
+
 done
 
 # SimuRepID = Simulation Replicate ID
 # Mu = mutation rate
 # RecRate = recombination rate (change the slim script if simulating multiple chromosomes)
-# LCh = length of chromosome 
-# BurninSize = size of the burnin populations (IF THIS NEEDS TO BE CHANGED, MAKE SURE TO CHANGE MUTATION RATE, RECOMBINATION RATE, AND NUMBER OF GENERATIONS ACCORDINGLY) 
+# LCh = length of chromosome
+# BurninSize = size of the burnin populations (IF THIS NEEDS TO BE CHANGED, MAKE SURE TO CHANGE MUTATION RATE, RECOMBINATION RATE, AND NUMBER OF GENERATIONS ACCORDINGLY)
 # BurninPath = path to the burnin files
 # BurninFilename = name of the burnin file
 
--- ShellScripts/Selection.sh
+++ ShellScripts/Selection.sh
@@ -2,48 +2,49 @@
 ## IMPORTANT: Change the output directory and in the first step and the variables in the second step before running.
 ## Run the script on server using: nohup bash Selection.sh > Selection.nohup &
 
+
+
+##### Set variables here:
+
+QTL_in=NQTL10
+SLN_NM=10_percent
+
+# still need to change variables below to match.
+
+
 ## Step 1: Ceate directories to store the outputs. Make sure to change the directory names in the first three lines.
 
-cd /fs/cbsubscb10/storage/rl683/TemporalScan/Simulations/ # Change this to a directory where you want to store all you simulation outputs.
-mkdir NQTL10 # Change this to what you want to name this particular quantitative trait architectures and/or the experimental design that you are simulating.
-cd NQTL10 # Same as above
-for k in {1..100} # Number of simulation replicates that you want to create.
+cd /users/r/b/rbrennan/evolve-resequence-simulation/simulations # Change this to a directory where you want to store all you simulation outputs.
+mkdir "$QTL_in" # Change this to what you want to name this particular quantitative trait architectures and/or the experimental design that you are simulating.
+cd "$QTL_in" # Same as above
+for k in {1..1} # Number of simulation replicates that you want to create.
 do
     mkdir 'SimRep'$k
     cd 'SimRep'$k
-    for j in {1..10}
-    do
-        mkdir 'ExpRepPlus'$j
-        mkdir 'ExpRepMinus'$j
-    done
+
+    # SET degree of selection! this must match below
+    mkdir "$SLN_NM"
+
     cd ..
 done
 cd ..
 
-## Step 2: Run burnin using SLiM 2. Variables inside the loop are all customizable and can be changed as desired. 
+## Step 2: Run burnin using SLiM 2. Variables inside the loop are all customizable and can be changed as desired.
 ## IMPORTANT: Number of generations has to be changed in the slim script.
-## It is recommended if you copy this shell script for each of the trait architecture x experimental combinations that you want to test and make changes in the new script. 
-## For example, when simulating the scenario with 100 QTLs, copy this file and rename it as NQTL100.sh. Set the NQTL variable to be 100 and run it with "nohup bash NQTL100.sh > NQTL100.nohup &" 
+## It is recommended if you copy this shell script for each of the trait architecture x experimental combinations that you want to test and make changes in the new script.
+## For example, when simulating the scenario with 100 QTLs, copy this file and rename it as NQTL100.sh. Set the NQTL variable to be 100 and run it with "nohup bash NQTL100.sh > NQTL100.nohup &"
 ## Note: The number of generations in the selection experiment can only be changed in the Selection.slim script.
-## Depending on the population size, more or fewer burnin generations might have been needed. If that's the case, edit the Selection.slim file as instructed in the file. 
+## Depending on the population size, more or fewer burnin generations might have been needed. If that's the case, edit the Selection.slim file as instructed in the file.
 
-for k in {1..100} # Set the number of simulation replicates that you want to create.
+for k in {1..1} # Set the number of simulation replicates that you want to create.
 do
     echo $k
-    for j in {1..1} # Set the number of experimental replications. 
-    do
-        echo $j
-        for i in {T,F} # Set the direction of selection (F if selecting the larger phenotype, T otherwise, T,F is both directions are selected)
-        do
-            echo $i
-            # Set the path to the SLiM program in the next line 
-            ~/Program/SLiM/bin/slim \
+            # Set the path to the SLiM program in the next line
+            ~/bin/SLiM/slim \
             -d SimRepID=$k  \
-            -d ExpRepID=$j \
-            -d Direction=$i \
-            -d "BurninPath='/fs/cbsubscb10/storage/rl683/TemporalScan/Simulations/Burnin/'" \
+            -d "BurninPath='/users/r/b/rbrennan/evolve-resequence-simulation/simulations/Burnin/'" \
             -d "BurninFilename='Burnin.txt'" \
-            -d LCh=30000000 \
+            -d LCh=20000000 \
             -d RecRate=1e-8 \
             -d SampleSize=50 \
             -d NQTL=10 \
@@ -52,17 +53,32 @@ do
             -d LowFreq=F \
             -d FreqBound=0.0 \
             -d LowerPosBound=0 \
-            -d UpperPosBound=29999999 \
+            -d UpperPosBound=19999999 \
             -d D=0.5 \
-            -d Epistasis=F \
-            -d "EpiSce=c(0,1,2,1,2,3,2,3,4)" \
-            -d PopSize=1000 \
-            -d SelectedSize=100 \
-            -d "OutPath='/fs/cbsubscb10/storage/rl683/TemporalScan/Simulations/NQTL10/'" \
-            /fs/cbsubscb10/storage/rl683/TemporalScan/SlimScripts/Selection.slim # Directory to the Selection.slim file included in the simulation tool.
+            -d OffspringSize=80000 \
+            -d ParentSize=50 \
+            -d Cutoff_Quantile=0.1 \
+            -d "Quantile_label='10_percent'" \
+            -d "OutPath='/users/r/b/rbrennan/evolve-resequence-simulation/simulations/NQTL10/'" \
+            /users/r/b/rbrennan/evolve-resequence-simulation/SlimScripts/Selection.slim # Directory to the Selection.slim file included in the simulation tool.
+
+        # reformat to save disc space
+        cd ~/evolve-resequence-simulation/simulations/$QTL_in/SimRep$k/$SLN_NM/
+
+        for i in $(ls | grep 'genomes' | cut -f 1-2 -d "_"); do
+
+            # save num of genomes remaining in each:
+            head -n 1 ${i}_genomes.txt | cut -f 4 -d " " | paste - >> number_genomes.txt
+
+            sed -n '/^Mutations/,/^Genomes/p' ${i}_genomes.txt | grep -v "^Genomes:" | grep -v "^Mutations" | grep -v 'm3' > ${i}_mutations.txt
+
+            #rm ${i}_genomes.txt
         done
+
     done
-done
+
+
+
 
 # SimuRepID = Simulation Replicate ID
 # ExpRepID = Experiment replicate ID
@@ -74,8 +90,8 @@ done
 # SampleSize = number of individuals to sample each generation
 # NQTL = number of QTLs, (even number is recommended when the number is small)
 # ESMean = absolute value of mean effect size
-# ESDist = effect size distribution("f" for fixed or "e" for exponential), 
-# LowFreq = starting frequency preference (T if selecting for lower frequency, F if selecting for higher frequency or random) 
+# ESDist = effect size distribution("f" for fixed or "e" for exponential),
+# LowFreq = starting frequency preference (T if selecting for lower frequency, F if selecting for higher frequency or random)
 # FreqBound = frequency bound (0.0~0.5, 0.0 if starting frequency is random)
 # LowerPosBound = lower position bound (0 if random)
 # UpperPosBound = upper position bound (LCh-1 if random)
